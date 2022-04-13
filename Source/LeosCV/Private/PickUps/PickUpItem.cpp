cdbfc3ce4600615be1cfcde9e5822ff41dfac7a6
leoroqueb <leoroqueb@gmail.com>
2022-04-13 21:40:08 +0100

    PickUps listos

--- Source/LeosCV/Private/PickUps/PickUpItem.cpp
+++ Source/LeosCV/Private/PickUps/PickUpItem.cpp
@@ -9,12 +9,79 @@ APickUpItem::APickUpItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ItemStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("ItemMesh"));
+	ItemPickUpRangeCollision = CreateDefaultSubobject<USphereComponent>(FName("PickUpRangeCollision"));
+	ItemMagneticRangeCollision = CreateDefaultSubobject<USphereComponent>(FName("MagneticRangeCollision"));
+	RotatingComponent = CreateDefaultSubobject<URotatingMovementComponent>(FName("RotatingComponent"));
+	ItemLight = CreateDefaultSubobject<USpotLightComponent>(FName("ItemLight"));
+	
+	ItemStaticMesh->SetupAttachment(RootComponent);
+	ItemPickUpRangeCollision->SetupAttachment(ItemStaticMesh);
+	ItemMagneticRangeCollision->SetupAttachment(ItemStaticMesh);
+	ItemLight->SetupAttachment(ItemStaticMesh);
+	
+	ItemStaticMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+}
+
+void APickUpItem::ActivateItem(const bool bActivate) const
+{
+	if(IsValid(this)){
+		ItemStaticMesh->SetVisibility(bActivate);
+		ItemMagneticRangeCollision->SetActive(bActivate);
+		ItemPickUpRangeCollision->SetActive(bActivate);
+		ItemLight->SetVisibility(bActivate);
+	}
+}
+
+void APickUpItem::OnItemPickUpRangeOverlap(UPrimitiveComponent* OverlappedComponent, 
+					  AActor* OtherActor, 
+					  UPrimitiveComponent* OtherComp, 
+					  int32 OtherBodyIndex, 
+					  bool bFromSweep, 
+					  const FHitResult &SweepResult )
+{
+	// Check actor who overlapped is player
+	ActorWhoPicked = Cast<ALeosCVCharacter>(OtherActor);
+	if(ActorWhoPicked)
+	{
+		if(RespawnTime > 0)
+		{
+			// Hide everything for 'RespawnTime' secs
+			ActivateItem(false);
+			
+			ItemEffect();
+			GetWorldTimerManager().SetTimer(RespawnableHandler, this, &APickUpItem::CheckActorIsNoLongerOverlapping,
+				GetWorld()->GetDeltaSeconds()/2, true, RespawnTime);
+			
+		}else
+		{
+			ItemEffect();
+			Destroy();
+		}
+	}
+}
+
+void APickUpItem::CheckActorIsNoLongerOverlapping()
+{
+	if(!IsOverlappingActor(ActorWhoPicked))
+	{
+		GetWorldTimerManager().ClearTimer(RespawnableHandler);
+		ActivateItem(true);
+	}
 }
 
 // Called when the game starts or when spawned
 void APickUpItem::BeginPlay()
 {
 	Super::BeginPlay();
+
+	ItemPickUpRangeCollision->OnComponentBeginOverlap.AddDynamic(this, &APickUpItem::OnItemPickUpRangeOverlap);
+
+	if(bRotates)
+	{
+		RotatingComponent->SetActive(true);
+	}
 	
 }
 
--- Source/LeosCV/Public/PickUps/PickUpItem.h
+++ Source/LeosCV/Public/PickUps/PickUpItem.h
@@ -3,21 +3,80 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "Components/SpotLightComponent.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/RotatingMovementComponent.h"
+#include "LeosCV/LeosCVCharacter.h"
 #include "PickUpItem.generated.h"
 
 UCLASS()
 class LEOSCV_API APickUpItem : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	APickUpItem();
 
+public:
+	//Actor components
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	USphereComponent* ItemPickUpRangeCollision;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	USphereComponent* ItemMagneticRangeCollision;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	UStaticMeshComponent* ItemStaticMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	URotatingMovementComponent* RotatingComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	USpotLightComponent* ItemLight;
+	
+	//Actor variables
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effects")
+	float EffectDuration;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="ItemSettings")
+	bool bIsMagnetic;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="ItemSettings")
+	bool bRotates;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="ItemSettings")
+	bool bUpAndDown;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="ItemSettings")
+	float RespawnTime;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="ItemData")
+	ALeosCVCharacter* ActorWhoPicked;
+
+	//Code variables
+	FTimerHandle RespawnableHandler;
+	
+	APickUpItem();
+private:
+
+	UFUNCTION()
+	void CheckActorIsNoLongerOverlapping();
+
+	UFUNCTION(BlueprintCallable, Category="ItemVisual")
+	void ActivateItem(bool bActivate) const;
+	
+	UFUNCTION(Category="ItemCollision")
+	void OnItemPickUpRangeOverlap(UPrimitiveComponent* OverlappedComponent, 
+					  AActor* OtherActor, 
+					  UPrimitiveComponent* OtherComp, 
+					  int32 OtherBodyIndex, 
+					  bool bFromSweep, 
+					  const FHitResult &SweepResult );
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void ItemEffect();
 
 public:	
 	// Called every frame
