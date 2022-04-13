587249f8137dbfef46e71ea77fa47d68f39900b5
leoroqueb <leoroqueb@gmail.com>
2022-04-13 15:01:33 +0100

    Primer commit carga de proyecto

---
+++ README.md
@@ -0,0 +1,3 @@
+# LeosCV
+
+Developed with Unreal Engine 5
---
+++ Source/LeosCV/LeosCVGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LeosCVGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALeosCVGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALeosCVGameMode();
+};
+
+
+
